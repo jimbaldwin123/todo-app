9c5c9adee6ee275e3ccf8122af64a3f03080cd33
Jim Baldwin <jim@hvaldwin.net>
2015-03-25 12:14:24 -0700

    push test rollback

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,7 @@
 					<span class="icon-bar"></span>
 				</button>
 
-				<a class="navbar-brand" href="/">Jim's xTODO List</a>
+				<a class="navbar-brand" href="/">Jim's TODO List</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
