8183d416b0242ac7fbc763a1d7214f85f69f1186
Jim Baldwin <jim@netvendor.net>
2019-11-10 07:02:55 -0800

    update to L 5.2

--- config/app.php
+++ config/app.php
@@ -108,17 +108,18 @@ return [
 	|
 	*/
 
+	// upgrade from 5.1 to 5.2
+	'env' => env('APP_ENV', 'production'),
+
 	'providers' => [
 
 		/*
 		 * Laravel Framework Service Providers...
 		 */
-		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
 		'Illuminate\Auth\AuthServiceProvider',
 		'Illuminate\Bus\BusServiceProvider',
 		'Illuminate\Cache\CacheServiceProvider',
 		'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
-		'Illuminate\Routing\ControllerServiceProvider',
 		'Illuminate\Cookie\CookieServiceProvider',
 		'Illuminate\Database\DatabaseServiceProvider',
 		'Illuminate\Encryption\EncryptionServiceProvider',
@@ -141,7 +142,6 @@ return [
 		 * Application Service Providers...
 		 */
 		'App\Providers\AppServiceProvider',
-		'App\Providers\BusServiceProvider',
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
